7d0af43ebbb35b1975e53bb5e03e779c7d6b16b7
BerkeYenerr <haydarberke_@hotmail.com>
2025-07-07 09:52:30 +0300

    qwe
    qweasdas

---
+++ Core/MoviApi.Domain/Entities/Cast.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviApi.Domain.Entities
+{
+    public class Cast
+    {
+        public int CastId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public String? Overview { get; set; }
+
+        public String? Biography { get; set; }
+
+
+    }
+}
